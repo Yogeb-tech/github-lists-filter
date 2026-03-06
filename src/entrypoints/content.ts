0fe85ae053428218f870d1041d18b2b1927bfdb1
Flavore669 <dirkosh2@gmail.com>
2026-03-05 19:53:36 -0600

    Revert "failure"
    This reverts commit 36b91154f0961a9b3bce517d93588ff8bd1f85f7.

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,23 +1,6 @@
-import { createApp } from "vue";
-import { createIntegratedUi } from "wxt/client";
-import { defineContentScript } from "wxt/sandbox";
-import App from "./App.vue";
-
 export default defineContentScript({
-  matches: ["<all_urls>"],
-  main(ctx) {
-    const ui = createIntegratedUi(ctx, {
-      position: "inline",
-      anchor: "body",
-      onMount: (container) => {
-        const app = createApp(App);
-        app.mount(container);
-        return app;
-      },
-      onRemove: (app) => {
-        app?.unmount();
-      },
-    });
-    ui.mount();
+  matches: ['*://*.google.com/*'],
+  main() {
+    console.log('Hello content.');
   },
 });
